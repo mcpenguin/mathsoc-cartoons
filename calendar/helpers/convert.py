4b46cce4dd1432fc8f83f85023e9a260bff1de63
Marcus Chan <marcus.chanwc@gmail.com>
2022-01-10 20:00:27 -0500

    init data

---
+++ calendar/helpers/convert.py
@@ -0,0 +1,24 @@
+import json
+# take todo.txt and convert it into respective json file
+
+filein = open('./todo.txt', 'r')
+# init list of events
+eventList = []
+# for each line in txt file, split based on colon
+for line in filein:
+    line = line.split(': ')
+    # line = [weeks, desc]
+    # convert weeks into num of weeks before publishing by
+    # adding -6 to each value, rounding to 1dp
+    line[0] = round(float(line[0]) - 6, 1)
+    # remove newline from line[1]
+    line[1] = line[1].strip()
+    # add to list of events
+    eventList.append(line)
+
+# write to json file
+with open('../comic_reminders.json', 'w') as outfile:
+    json.dump(eventList, outfile)
+
+outfile.close()
+
